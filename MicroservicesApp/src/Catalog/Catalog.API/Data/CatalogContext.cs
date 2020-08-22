57549b1dd5bf62e1ebb3d91ff340ae1fab796270
siemens-c2c <joy_mon78@yahoo.co.in>
2020-08-22 23:55:07 +0530

    Recreated the application
    There was mistake in organization of folder structure

---
+++ MicroservicesApp/src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Data.Intarfaces;
+using Catalog.API.Entities;
+using Catalog.API.Settings;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(ICatalogDatabaseSettings setting)
+        {
+            try
+            {
+                var client = new MongoClient(setting.ConnectionString);
+                var db = client.GetDatabase(setting.DatabaseName);
+                Products = db.GetCollection<Product>(setting.CollectionName);
+                CatalogContextSeed.SeedData(Products);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ MicroservicesApp/src/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,132 @@
+﻿using Catalog.API.Data.Intarfaces;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            try
+            {
+                _context = context ?? throw new ArgumentNullException(nameof(context)); ;
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            try
+            {
+                return await _context.Products.Find(p => true).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        //issue
+        public async Task<bool> Delete(string id)
+        {
+            FilterDefinition<Product> findProductFilter = Builders<Product>.Filter.Eq("Id", id);
+            try
+            {
+                DeleteResult deleteResult = await _context.Products.DeleteOneAsync(findProductFilter);
+                return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            //FilterDefinition<Product> findByIdFilter = Builders<Product>.Filter.ElemMatch(p => p.Id, id);
+            try
+            {
+                //return await _context
+                //            .Products
+                //            .Find(findByIdFilter)
+                //            .FirstOrDefaultAsync();
+                return await _context
+                          .Products
+                          .Find(p=>p.Id==id)
+                          .FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filterByCategory = Builders<Product>.Filter.Eq("Category", categoryName);
+            try
+            {
+                return await _context.Products.Find(filterByCategory).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filterByName = Builders<Product>.Filter.Eq("Name", name);
+            try
+            {
+                return await _context.Products.Find(filterByName).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Create(Product product)
+        {
+            try
+            {
+                await _context.Products.InsertOneAsync(product);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool> Update(Product product)
+        {
+            try
+            {
+                ReplaceOneResult updateResult = await _context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+                return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
