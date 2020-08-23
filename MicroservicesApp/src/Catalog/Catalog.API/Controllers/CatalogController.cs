dd8dba0c979b9293513cdd9bb3651d15700fde1e
siemens-c2c <joy_mon78@yahoo.co.in>
2020-08-23 10:32:50 +0530

    Microservices App Updated

--- MicroservicesApp/src/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ MicroservicesApp/src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -8,6 +8,7 @@ using Catalog.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 
 namespace Catalog.API.Controllers
 {
@@ -36,6 +37,72 @@ namespace Catalog.API.Controllers
                 return NotFound();
             }
             return Ok(products);
-        }        
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProduct(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product is not found in database.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [Route("[action]/{productName}")]
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string productName)
+        {
+            var products = await _repository.GetProductByName(productName);
+            if (products == null || products.Count() == 0)
+            {
+                _logger.LogError($"Products are not found in database with {productName}");
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        [Route("[action]/{categoryName}")]
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategoryName(string categoryName)
+        {
+            var products = await _repository.GetProductByCategory(categoryName);
+            if (products == null || products.Count() == 0)
+            {
+                _logger.LogError($"Products are not found in database with {categoryName}");
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateProduct([FromBody]Product product)
+        {
+            await _repository.Create(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody]Product product)
+        {
+            return Ok(await _repository.Update(product));
+        }
+
+        [HttpDelete("{productId:length(24)}")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(string productId)
+        {
+            return Ok(await _repository.Delete(productId));
+        }
     }
 }
